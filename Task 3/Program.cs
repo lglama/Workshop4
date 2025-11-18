0ec7cbebad0be1f4dd1741c1d61105e02abc755e
lglama <laneelama@gmail.com>
2025-11-18 11:58:01 +0545

    Task 3

---
+++ Task 3/Program.cs
@@ -0,0 +1,13 @@
+﻿using Task_3;
+
+ParameterDemo pd = new ParameterDemo();
+
+int value = 5;
+pd.Increase(ref value);
+Console.WriteLine("After Increase: " + value);
+
+pd.GetFullName(out string myname);
+Console.WriteLine("Full Name: " + myname);
+
+int total = pd.SumAll(1, 2, 3, 4);
+Console.WriteLine("Sum: " + total);
