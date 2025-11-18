a8e12799a248614219baa044779de2125d9a76c2
lglama <laneelama@gmail.com>
2025-11-18 12:02:25 +0545

    Task 4

---
+++ Task 4/Program.cs
@@ -0,0 +1,7 @@
+﻿using Task_4;
+
+Player p1 = new Player();
+Console.WriteLine("Player 1 -> Name: " + p1.playerName + ", Level: " + p1.level + ", Health: " + p1.health);
+
+Player p2 = new Player("Lanee", 7, 120);
+Console.WriteLine("Player 2 -> Name: " + p2.playerName + ", Level: " + p2.level + ", Health: " + p2.health);
