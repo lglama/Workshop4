6bf9f9208e815044a49a91ae53db77c1acdc0348
lglama <laneelama@gmail.com>
2025-11-19 11:12:32 +0545

    Task 6

---
+++ Task 6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter marks: ");
+        int.TryParse(Console.ReadLine(), out int marks);
+
+        Console.Write("Enter total: ");
+        int.TryParse(Console.ReadLine(), out int total);
+
+        double percentage = (double)marks / total * 100;
+
+        Console.WriteLine("Percentage: " + percentage);
+    }
+}
